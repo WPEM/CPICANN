23ab1b2b50e07869c8aa2caf42565cd20cd704f3
Zhang Shouyang <997557235@qq.com>
2024-04-14 16:25:27 +0800

    Delete source_code directory

--- source_code/__init__.py
+++
@@ -1,6 +0,0 @@
-
-__description__ = 'CPICANN : Crystallographic Phase Identifier of Convolutional self-Attention Neural Network'
-__documents__ = ' https://github.com/WPEM/CPICANN'
-__email__ = 'binjacobcao@gmail.com'
-              
-
